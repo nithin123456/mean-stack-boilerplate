93a28ac3ca83da6caaf6317d938fe651fbc34f3b
divyanshu <mittal_divyanshu@yahoo.com>
2016-09-01 01:15:02 -0500

    master: contact even rep and benefits package UI

---
+++ public/qualify-candidate/contact-egen/contact-egen.controller.js
@@ -0,0 +1,13 @@
+(function (angular) {
+    'use strict';
+
+    angular
+        .module('app.qualifyCandidate')
+        .controller('ContactEgenController', ContactEgenController);
+
+    // @ngInject
+    function ContactEgenController() {
+        var vm = this;
+
+    }
+}(angular));
