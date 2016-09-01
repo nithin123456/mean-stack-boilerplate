0880dc0862ae15023a1520e4d5c931ab09c5fd8b
divyanshu <mittal_divyanshu@yahoo.com>
2016-09-01 01:42:19 -0500

    master: use mailto client

--- public/qualify-candidate/contact-egen/contact-egen.controller.js
+++ public/qualify-candidate/contact-egen/contact-egen.controller.js
@@ -6,8 +6,13 @@
         .controller('ContactEgenController', ContactEgenController);
 
     // @ngInject
-    function ContactEgenController() {
+    function ContactEgenController($window) {
         var vm = this;
+        var email = 'trevor.benson@egeni.com';
+        vm.send = send;
 
+        function send() {
+            $window.location.href = 'mailto:' + email + '?subject=' + vm.candidateName + ' - ' + vm.candidateEmail + '&body=' + vm.message;
+        }
     }
 }(angular));
