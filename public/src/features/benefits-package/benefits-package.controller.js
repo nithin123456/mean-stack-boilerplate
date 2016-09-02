7fd6ed0fc3d1b682d76739baf9525c12312a514f
divyanshu <mittal_divyanshu@yahoo.com>
2016-09-01 23:35:44 -0500

    master: add s3 url to email body

--- public/src/features/benefits-package/benefits-package.controller.js
+++ public/src/features/benefits-package/benefits-package.controller.js
@@ -9,19 +9,20 @@
     function BenefitsPackageController($window) {
         var vm = this;
         var subject = 'Egen Solutions/Insight Global benefits';
-        var body = 'Thank you for taking the time to speak  with us. Please find General information about Egen Solutions and their H1 transfer process,' +
-            ' along with Healthcare Benefits attached. If you have any questions regarding this, please contact Trevor, who’s information can be found below.' +
-            'Trevor Benson- Sales/Partnerships' +
-            'trevor.benson@egeni.com (815) 762-8659' +
-            'www.egen.solutions  Naperville- IL';
-
+        var body = 'Thank you for taking the time to speak  with us. Please find General information about Egen Solutions ' +
+            'and their H1 transfer process, along with Healthcare Benefits. \n' +
+            ' If you have any questions regarding this, please contact Trevor, who’s information can be found below. \n\n' +
+            'Trevor Benson- Sales/Partnerships \n' +
+            'trevor.benson@egeni.com (815) 762-8659' + '\n \n' +
+            'https://s3.amazonaws.com/insight-egen/Egen-IG-Candidate.pdf' +'\n' +
+        'https://s3.amazonaws.com/insight-egen/2016+Egen+Benefit+Summary+-+External+2500+Global+Transfer.pdf';
         vm.send = send;
 
         function send() {
             $window.location.href =
                 'mailto:' + vm.candidateEmail +
                 '?subject=' + subject +
-                '&body=' + (vm.message ? vm.message : body);
+                '&body=' + (vm.message ? vm.message : encodeURIComponent(body));
         }
 
     }
